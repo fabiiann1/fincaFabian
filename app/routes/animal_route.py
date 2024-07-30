6a364a80f062bb7885847207e4f30ff156e04627
fabiiann1 <fabianmarinn1@gmail.com>
2024-07-30 11:16:03 -0500

    rutass

---
+++ app/routes/animal_route.py
@@ -0,0 +1,59 @@
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+
+bp = Blueprint('animal', __name__)
+
+@bp.route('/Animal')
+def index():
+    from app.models.animal import Animal
+    data = Animal.query.all()
+    return render_template('animales/index.html', data=data)
+
+@bp.route('/Animal/add', methods=['GET', 'POST'])
+def add():
+    from app.models.animal import Animal
+    from app import db
+
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        especie = request.form['especie']
+        raza = request.form['raza']
+        edad = request.form['edad']
+    
+        new_animales = Animal(nombre=nombre,especie=especie,raza=raza,edad=edad)
+        db.session.add(new_animales)
+        db.session.commit()
+        
+        return redirect(url_for('animal.index'))
+
+    return render_template('animales/add.html')
+
+@bp.route('/Animal/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+
+    from app.models.animal import Animal
+    from app import db
+
+    actor = Animal.query.get_or_404(id)
+
+    if request.method == 'POST':
+
+        actor.nombre = request.form['nombre']
+        
+
+        db.session.commit()
+        return redirect(url_for('animal.index'))
+
+    return render_template('animales/edit.html', actor=actor)
+    
+
+@bp.route('/Animal/delete/<int:id>')
+def delete(id):
+    from app.models.animal import Animal
+    from app import db
+    animal = Animal.query.get_or_404(id)
+    
+    db.session.delete(animal)
+    db.session.commit()
+
+    return redirect(url_for('animal.index'))
